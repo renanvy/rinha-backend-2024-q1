c9ab32ee05a8b5a79ec12ee446bf74b06278d71e
Norberto Oliveira Junior <norbejunior@gmail.com>
2024-02-10 17:06:44 -0300

    Set port config getting from env var

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :rinha,
-  port: 4000
+  port: System.get_env("PORT", "4000")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
