68da35e48d08627fe11c157e0b0ba166312fed7c
Norberto Oliveira Junior <norbejunior@gmail.com>
2024-02-13 01:10:58 -0300

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :rinha,
   port: System.get_env("PORT", "4000"),
-  nodes: [:"rinha1@Norbertos-MacBook-Pro-3", :"rinha2@Norbertos-MacBook-Pro-3"]
+  nodes: [:"rinha1@localhost", :"rinha2@localhost"]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
