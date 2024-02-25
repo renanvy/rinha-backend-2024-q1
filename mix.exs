3cc68dffdcdc390be637305b378a035d89dfa1e8
Renan <renanvy@gmail.com>
2024-02-24 23:04:24 -0300

    Fixa a versão do Elixir e Erlang

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rinha.MixProject do
     [
       app: :rinha,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.16.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
